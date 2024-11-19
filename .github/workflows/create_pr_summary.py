1043dd69f7e7a435f72b4995d938aefc014c084a
Markus Mühlhäußer <markus.muehlhaeusser@tum.de>
2024-11-19 14:22:20 +0100

    Add ai python script

---
+++ .github/workflows/create_pr_summary.py
@@ -0,0 +1,13 @@
+import google.generativeai as genai
+import os
+import sys
+
+api_key = os.environ.get('GEMINI_API_KEY')
+genai.configure(api_key)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+prompt = sys.stdin.read()
+# print(prompt)
+response = model.generate_content("Create a concise summary in markdown format of the following git diff of a pull request: \n" + prompt)
+print(response.text)
+
